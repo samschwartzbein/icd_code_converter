172ebf8c0ce566a10c3a0bb16c7e487f66a49bb6
samschwartzbein <87439310+samschwartzbein@users.noreply.github.com>
2022-06-13 15:14:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ class ExampleModel(BaseModel):
 def main():
     """ICD Converter App"""
     st.title("Medical Code to SAS Converter")
-    st.markdown("###### This application transforms lists of medical codes (ICD9, ICD10, CPT, HSPS, GPI) into a SAS readable format")
+    st.markdown("###### This application transforms medical codes (ICD9, ICD10, CPT, HSPS, GPI) into a SAS readable format")
     data = sp.pydantic_form(key="my_form", model=ExampleModel)
 
     if data:
