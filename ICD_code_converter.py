c4f74ba34d7786df09b03f87250bf9b337650f22
samschwartzbein <87439310+samschwartzbein@users.noreply.github.com>
2022-06-13 12:03:47 -0400

    Update ICD_code_converter.py

--- ICD_code_converter.py
+++ ICD_code_converter.py
@@ -119,11 +119,11 @@ def print_codes(clean_codes):
         if i % 10 != 0:
             to_print.append('"' + clean_codes[i] + '",')
         if i % 10 == 0:
-            st.code(to_print)
-            to_print = []
+            to_print.append("\n")
             to_print.append('"' + clean_codes[i] + '",')
         if i == len(clean_codes) - 1:
             to_print[-1] = to_print[-1].replace(",", "")
-            st.code(to_print)
+            final = ' '.join([str(item) for item in to_print])
+            st.code(final + ");")
             st.code(");")
     return
