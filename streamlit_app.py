2a22bc8c82d1f67f96cce7dd36ace76b85b0732a
Sam Schwartzbein <87439310+samschwartzbein@users.noreply.github.com>
2023-08-23 15:36:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,24 @@ import streamlit as st
 import pydantic
 # from pydantic import BaseModel
 print(pydantic.__version__)
-import streamlit_pydantic as sp
-from ICD_code_converter import *
+print("hello")
+# import streamlit_pydantic as sp
+# from ICD_code_converter import *
 
-class ExampleModel(BaseModel):
-    Enter_Your_Codes: str
+# class ExampleModel(BaseModel):
+#     Enter_Your_Codes: str
 
-def main():
-    """ICD Converter App"""
-    st.title("Medical Code to SAS Converter")
-    st.markdown("###### This application transforms medical codes (ICD9, ICD10, CPT, HSPS, GPI) into a SAS readable format")
-    data = sp.pydantic_form(key="my_form", model=ExampleModel)
+# def main():
+#     """ICD Converter App"""
+#     st.title("Medical Code to SAS Converter")
+#     st.markdown("###### This application transforms medical codes (ICD9, ICD10, CPT, HSPS, GPI) into a SAS readable format")
+#     data = sp.pydantic_form(key="my_form", model=ExampleModel)
 
-    if data:
-        userInput = data.Enter_Your_Codes
-        cleaned = clean_codes(userInput)
-        final = print_codes(cleaned)
-        st.code(final)
-    pass
+#     if data:
+#         userInput = data.Enter_Your_Codes
+#         cleaned = clean_codes(userInput)
+#         final = print_codes(cleaned)
+#         st.code(final)
+#     pass
 
-main()
+# main()
